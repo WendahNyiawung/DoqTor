c13e07675adfd5d27b0124cb623bc6f87297aa54
WendahNyiawung <42493128+WendahNyiawung@users.noreply.github.com>
2020-06-11 10:33:05 +0100

    'initial'

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      'sm': {'min': '320px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px'},
+    },
+  },
+  variants: {},
+  plugins: [],
+}
